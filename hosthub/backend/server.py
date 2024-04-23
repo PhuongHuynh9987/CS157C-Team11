786be628ee44be78bf3d6fcde907d95e4568ae2f
Elena Ilic <125409165+eilici@users.noreply.github.com>
2024-04-23 13:23:23 -0700

    remove more duplicates and fix server

--- hosthub/backend/server.py
+++ hosthub/backend/server.py
@@ -1,9 +1,7 @@
-from flask import Flask, jsonify, request, redirect, url_for,send_from_directory
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 import redis
-from Models import User
-from Models import Host
+from Models import User, Booking, Host
 from pydantic import ValidationError
 from redis_om import Migrator
 from flask_jwt_extended import create_access_token
@@ -18,17 +16,14 @@ import os
 from datetime import datetime
 from werkzeug.utils import secure_filename
 import requests
-from flask import current_app
+from flask import Flask, current_app, jsonify, request
 import json
 
 # from redis.cluster import RedisCluster
 
-
-
 r = redis.Redis(host = 'localhost', port = 6379, decode_responses = True, db=0)
 # r = redis.Redis(host='redis-13404.c14.us-east-1-2.ec2.cloud.redislabs.com', 
             # port=13404, db=0, password="n3pRbBl2Y9oMdJ7J36QTPHzJBPPSAaQQ")
-
 # rc = RedisCluster(host='localhost', port=16379)
 
 # ############ create Flask app
@@ -50,23 +45,29 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # print(pathName)
 app.config['STATIC_FOLDER'] = 'uploads'
+
 # print(app.config.get('STATIC_FOLDER'))
 ######### enable Cors
 # CORS(app, resources ={r'/*':{'origins': 'http://localhost:5173'},'supports_credentials': True})
 CORS(app, origins = ['http://localhost:5173'],supports_credentials = True )
-
 
 # Api route
 @app.route("/",methods = ["GET"])
 def members():
     return 'localhost:5000'+pathName+'1711999951.jpg'
 
-
+# register a user account
 @app.route("/register",methods = ["POST"])
 def register():
     input =  request.get_json()
+    if not input:
+        return "Bad Request", 400
+
+    # Check for pre-existing account here
+
     pw_hash = bcrypt.generate_password_hash(input["password"],5).decode('utf-8')
     try: 
+        print(input["username"])
         person = User.User(
             username = input["username"],
             firstName =  input["firstName"],
@@ -76,19 +77,21 @@ def register():
         )
         person.save()
         return {"user_id": person.pk}
-
     except ValidationError as e:
         print(e)
 
+# log into user account
 @app.route("/login",methods = ["POST"])
 def login():
     input =  request.get_json()
     userData = User.User.find(User.User.username == input["username"])
-   
+
+    if not userData:
+     return ("No user found",404)
+
     if len(list(userData)) != 0:
         user = userData[0]
         hostData = Host.Host.find(Host.Host.owner == user.pk)
-
         checkPassword = bcrypt.check_password_hash(user.password, input["password"])
         if not checkPassword:
             return "wrong password"
@@ -97,23 +100,27 @@ def login():
                 response = jsonify({"username":userData[0].username,
                                     "firstName": userData[0].firstName,"lastName": userData[0].lastName,
                                      "id":userData[0].pk,"email":userData[0].email, 
-                                     "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc})
+                                     "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc,
+                                     "history":redis.execute_command(f"lrange history_{user.pk} 0 -1")})
             else:
                 response = jsonify({"username":userData[0].username,"firstName": userData[0].firstName,
                                     "lastName": userData[0].lastName, "id":userData[0].pk,"hostId":hostData[0].pk,
-                                    "email":userData[0].email, "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc})
+                                    "email":userData[0].email, "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc,
+                                    "history":redis.execute_command(f"lrange history_{user.pk} 0 -1")})
             access_token = create_access_token(identity=user.pk)
             set_access_cookies(response,access_token)
             return response
     else: 
         return "user not found"
 
+# log out of user account
 @app.route("/logout", methods = ["POST"] )
 def logout():
     response = jsonify({'logout': True})
     unset_jwt_cookies(response)
     return response
 
+# view profile details
 @app.route("/profile",methods = ["GET"])
 @jwt_required()
 def profile():
@@ -129,14 +136,13 @@ def profile():
         if (len(list(hostData)) == 0):
             return  {"username":userData[0].username,"firstName": userData[0].firstName,
                         "lastName": userData[0].lastName, "id":userData[0].pk, 
-                        "email":userData[0].email, "profilePhoto": userData[0].profilePhoto,
-                        "desc": userData[0].desc,"gender": userData[0].gender,"status": userData[0].status}
+                        "email":userData[0].email, "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc}
         else:
             return  {"username":userData[0].username,"firstName": userData[0].firstName,
                     "lastName": userData[0].lastName, "id":userData[0].pk,"hostId":hostData[0].pk,
-                    "email":userData[0].email, "profilePhoto": userData[0].profilePhoto, 
-                    "desc": userData[0].desc,"gender": userData[0].gender,"status": userData[0].status}
+                    "email":userData[0].email, "profilePhoto": userData[0].profilePhoto, "desc": userData[0].desc}
 
+# update profile details
 @app.route("/updateProfile", methods = ["PUT"])
 def update_profile():
     input = request.get_json()
@@ -151,17 +157,14 @@ def update_profile():
             email = input["email"],
             profilePhoto = input["uploadedPhoto"],
             desc = input["desc"],
-            password = hostData[0].password,
-            gender = input["gender"],
-            status = input["status"],
+            password = hostData[0].password
         )
         person.save()
         return {"user_id": person.pk}
-
     except ValidationError as e:
         print(e)
-    
 
+# create new hosting
 @app.route("/host",methods = ["POST"])
 def hosting():
     input = request.get_json()
@@ -172,17 +175,18 @@ def hosting():
             desc = input["desc"],
             address = input["address"],
             city =  input["city"],
-            state = input["state"],
+            state =  input["state"],
             zip = input["zip"],
             uploadedPhotos = input["uploadedPhotos"],
-            perks = input["perks"]
+            perks = input["perks"],
         )
-        host.save()
+        # host.save()
         return {"host_id": host.pk}
 
     except ValidationError as e:
         print(e)
 
+# update hosting details
 @app.route("/host",methods = ["PUT"])
 def hosting_update():   
     input = request.get_json()
@@ -190,7 +194,6 @@ def hosting_update():
     Migrator().run()   
     hostInfo = list(hostInfo)[0]
     hostId = hostInfo.pk
-
     try: 
         host = Host.Host(
             pk = hostId,
@@ -199,10 +202,10 @@ def hosting_update():
             desc = input["desc"],
             address = input["address"],
             city =  input["city"],
-            state = input["state"],
+            state =  input["state"],
             zip = input["zip"],
             uploadedPhotos = input["uploadedPhotos"],
-            perks = input["perks"]
+            perks = input["perks"],
         )
         host.save()
         return {"host_id": host.pk}
@@ -210,7 +213,7 @@ def hosting_update():
     except ValidationError as e:
             print(e)
 
-
+# see all hostings
 @app.route('/fetch_allHost', methods = ["GET"])
 def fetch_allHost():
     hosts = Host.Host.find().all()
@@ -219,53 +222,87 @@ def fetch_allHost():
         host_list.append(dict(i))
     return host_list
 
-   
+# see hosting info
 @app.route('/hostingInfo', methods = ["GET"])
 def get_hosting_info():
     verify = verify_jwt_in_request()
     current_user = get_jwt_identity()
     hostData = Host.Host.find(Host.Host.owner == current_user) 
-    return {"id": hostData[0].pk,"desc": hostData[0].desc,
-               "address": hostData[0].address,"city":hostData[0].city,"state":hostData[0].state, 
-                "zip":hostData[0].zip, "uploadedPhotos": hostData[0].uploadedPhotos, 
-                'title':hostData[0].title, 'perks': hostData[0].perks}
-                
+    return {"id": hostData[0].pk,"desc": hostData[0].desc, "address":hostData[0].address,
+               "city": hostData[0].city,"state":hostData[0].state, "zip":hostData[0].zip, 
+                "uploadedPhotos": hostData[0].uploadedPhotos, 
+                'title':hostData[0].title, 'perks': hostData[0].perks, "available":redis.execute_command(f"smembers available_{hostData.pk}")}
 
 @app.route('/hostingInfo', methods = ["POST"])
 def individual_host_info():
     input = request.get_json()
     hostData = Host.Host.find(Host.Host.pk == input["id"]) 
-    return {"id": hostData[0].pk,"desc": hostData[0].desc, "addressNumber":hostData[0].addressNumber,
-               "addressStreet": hostData[0].addressStreet,"cityStateZip":hostData[0].cityStateZip, 
-                "uploadedPhotos": hostData[0].uploadedPhotos, 'title':hostData[0].title}
+    return {"id": hostData[0].pk,"desc": hostData[0].desc, "address":hostData[0].address,
+               "city": hostData[0].city,"state":hostData[0].state, "zip":hostData[0].zip, 
+                "uploadedPhotos": hostData[0].uploadedPhotos, 
+                'title':hostData[0].title, 'perks': hostData[0].perks, "available":redis.execute_command(f"smembers available_{hostData.pk}")}
 
+# add a potential booking to user cart
+@app.route('/addToCart', methods = ["POST"])
+def add_cart():
+    current_user = get_jwt_identity()
+    id = current_user.pk
+
+    # check for cart hash by user id
+    cart_status = redis.execute_command(f"EXISTS cart_{id}")
+
+    # take host id and available
+    input = request.get_json()
+
+    if cart_status:
+        return ("Cart already occupied.")
+    else:
+        host_id = input["host_id"]
+        available = input["available"]
+        redis.execute_command(f"hmset cart_{id} host_id {host_id} available {available}")
+
+# empty the user's cart
+@app.route("/clearCart", methods = ["POST"])
+def clear_cart():
+    current_user = get_jwt_identity()
+    id = current_user.pk
+    redis.execute_command(f"delete cart_{id}")
+    return("Cart emptied.")
+
+# execute booking
+@app.route('/book', methods = ["POST"])
+def make_booking():
+    current_user = get_jwt_identity()
+    id = current_user.pk
+    # check for cart hash by user id
+    cart_status = redis.execute_command(f"EXISTS cart_{id}")
+
+    if cart_status:
+        try:
+            host_id = redis.execute_command(f"hget cart_{id} host_id")
+            date = redis.execute_command(f"hget cart_{id} available")
+
+            # create booking
+            booking = Booking.Booking(
+                user = id,
+                host = host_id,
+                available = date
+            )
+            booking.save()
+
+            # add booking to history for user and host+ remove from available
+            try: 
+                redis.execute_command(f"lpush history_{id} {booking.pk}")
+                redis.execute_command(f"lpush history_{host_id} {booking.pk}")
+                # remove from available here
+                return("Booking complete")
+            except Exception as e:
+                print(e)
+
+        except Exception as e:
+            print(e)
+    return ("Booking failed")
 
 @app.route('/uploads/<path:filename>', methods = ["GET"])
 def photoDisplay(filename):
-    return send_from_directory(app.config.get('STATIC_FOLDER'), filename)
-
-@app.route("/upload-by-link",methods = ["POST"])
-def upload_file_url():
-    input =  request.get_json()
-    image_url = input['link']
-    data = requests.get(image_url).content
-    now = datetime.now()
-    timestamp = str(round(datetime.timestamp(now)))
-    filename = timestamp + '.jpg'
-    f = open(pathName+'/'+filename,'wb')
-    f.write(data)
-    f.close()
-    return filename
-
-
-@app.route('/upload', methods = ["POST"])
-def upload_file():
-    file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return file.filename
-
-if __name__ == "__main__":
-    app.run(debug=True, port = 5000, host = "localhost")
-
-
-
+    return ## temporary
